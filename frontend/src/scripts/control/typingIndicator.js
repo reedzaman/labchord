02f424280284994825522373008078ef938406c5
Reed Zaman <reedzaman98@gmail.com>
2022-07-19 23:38:10 +0600

    added typing indication

---
+++ frontend/src/scripts/control/typingIndicator.js
@@ -0,0 +1,42 @@
+import {socket} from './client.js';
+
+let writing = false;
+
+// checks if user is writing
+// ... if writing, emits 'typing' once (no repeating emitting)
+// ... if not, emits 'stoppedTyping' once (no repeating emitting)
+document.addEventListener('keyup', ()=>{
+    if(msgInput.value.length > 0){
+        if(!writing){
+            writing = true;
+            socket.emit('typing');
+        }
+    } else {
+        if(writing){
+            writing = false;
+            socket.emit('stoppedTyping');
+        }
+    }
+});
+
+
+// if the focus shifts from the input box,
+// emits 'stoppedTyping'
+msgInput.addEventListener('focusout', () => {
+    if(writing){
+        writing = false;
+        socket.emit('stoppedTyping');
+    }
+
+})
+
+// if the focus returns to the input box (previously at writing state),
+// emits 'typing'
+msgInput.addEventListener('focusin', () => {
+    if(msgInput.value.length > 0){
+        if(!writing){
+            writing = true;
+            socket.emit('typing');
+        }
+    }
+});
