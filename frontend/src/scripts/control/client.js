62a29ce7a1e36c3ac26d5878e0c8d2231be60077
Reed Zaman <reedzaman98@gmail.com>
2022-07-21 13:18:15 +0600

    setting up bundler for whole frontend

--- frontend/src/scripts/control/client.js
+++ frontend/src/scripts/control/client.js
@@ -1,7 +1,7 @@
-import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+const client = require('socket.io-client');
 
 // using live-server
-const socket = io('http://127.0.0.1:3000');
+const socket = client.io('http://127.0.0.1:3000');
 
 socket.on("connect", () => {
   console.log('connected with socket id : ' + socket.id);
