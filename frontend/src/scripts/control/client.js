da8d936e9c97ade9dc56caa44f368f072d198e84
Reed Zaman <reedzaman98@gmail.com>
2022-07-21 22:25:38 +0600

    server setup

--- frontend/src/scripts/control/client.js
+++ frontend/src/scripts/control/client.js
@@ -1,7 +1,7 @@
-const client = require('socket.io-client');
+const { io } = require('socket.io-client');
 
 // using live-server
-const socket = client.io('http://127.0.0.1:3000');
+const socket = io('http://127.0.0.1:3000');
 
 socket.on("connect", () => {
   console.log('connected with socket id : ' + socket.id);
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const { Server } = require("socket.io");
+
+let usernames = {}
+
+const io = new Server(3000, {
+    cors: {
+        origin: "http://127.0.0.1:8080",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    const id = socket.id.replace(/[^a-zA-Z0-9]/g, '').replace(/^[0-9]+/, '');;
+
+    socket.on('send', (msgData, type) => {
+
+        if(type === 'text')
+            socket.broadcast.emit('msg', msgData, usernames[id], id, 'text');
+        else 
+            socket.broadcast.emit('msg', msgData, usernames[id], id, 'image');
+
+    })
+    
+    socket.on('setName', (name) => {
+        if(!usernames.hasOwnProperty(socket.id)){
+            //console.log("username is new");
+            // removing special characters from socket id
+            // storing usernames as (key::socket - value::name) pair
+            usernames[id] = name;
+            socket.broadcast.emit('notification', `${name} Joined the chat`);
+        }
+    })
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', id, usernames[id]);
+    })
+
+    socket.on('stoppedTyping', (name) => {
+        socket.broadcast.emit('stoppedTyping', id, usernames[id]);
+    })
+
+    socket.on('disconnect', reason => {
+        socket.broadcast.emit('stoppedTyping', id, usernames[id]);
+        socket.broadcast.emit('notification', `${usernames[id]} left the chat`);
+        delete usernames[id];
+    })
+});
